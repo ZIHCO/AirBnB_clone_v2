0b0a3ff3ecf45e4c43a05c586ee450d87c434e57
David Ukelere <david.ukelere291@gmail.com>
2023-07-08 14:51:00 +0100

    Added 1-3

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,30 +1,76 @@
 #!/usr/bin/python3
 """
-A Fabric script (based on the file 3-deploy_web_static.py)
-that deletes out-of-date archives, using the function do_clean
+Deletes out-of-date archives,
+using the function do_clean
 """
+from fabric.api import *
+from datetime import datetime
 import os
-from fabric.api import cd, env, local, run
 
-env.hosts = ["34.231.110.206", "3.239.57.196"]
+env.hosts = ['54.144.136.64', '100.24.236.179']
+env.user = 'ubuntu'
+
+
+def deploy():
+    ''' Deploys archive '''
+    archive_path = do_pack()
+    if not archive_path:
+        return False
+    return do_deploy(archive_path)
+
+
+def do_pack():
+    '''
+    Generates a tgz archive from the
+    contents of the web_static folder
+    '''
+    try:
+        local('mkdir -p versions')
+        datetime_format = '%Y%m%d%H%M%S'
+        archive_path = 'versions/web_static_{}.tgz'.format(
+            datetime.now().strftime(datetime_format))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        print('web_static packed: {} -> {}'.format(archive_path,
+              os.path.getsize(archive_path)))
+        return archive_path
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    '''
+    Deploy archive to web server
+    '''
+    if not os.path.exists(archive_path):
+        return False
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
+    try:
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(releases_path))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
+        run('rm -rf {}/web_static'.format(releases_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(releases_path))
+        print('New version deployed!')
+        return True
+    except:
+        return False
 
 
 def do_clean(number=0):
-    """
-    Deletes out-of-date archives
-    Args:
-        number: is the number of the archives, including the most recent
-    """
-    n = 1 if int(number) == 0 else int(number)
-    files = [f for f in os.listdir('./versions')]
-    files.sort(reverse=True)
-    for f in files[n:]:
-        local("rm -f versions/{}".format(f))
-    remote = "/data/web_static/releases/"
-    with cd(remote):
-        tgz = run(
-            "ls -tr | grep -E '^web_static_([0-9]{6,}){1}$'"
-        ).split()
-        tgz.sort(reverse=True)
-        for d in tgz[n:]:
-            run("rm -rf {}{}".format(remote, d))
+    ''' Removes out of date archives locally and remotely '''
+    number = int(number)
+    if number == 0:
+        number = 2
+    else:
+        number += 1
+
+    local('cd versions; ls -t | tail -n +{} | xargs rm -rf'
+          .format(number))
+    releases_path = '/data/web_static/releases'
+    run('cd {}; ls -t | tail -n +{} | xargs rm -rf'
+        .format(releases_path, number))
