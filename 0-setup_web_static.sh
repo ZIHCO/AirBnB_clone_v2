9dfdaf10454a2ab8b7adbead7b193feff44771ea
ZIHCO <chizobajames21@gmail.com>
2023-07-18 21:41:33 +0100

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,48 +1,36 @@
 #!/usr/bin/env bash
 # Script sets up web servers for deployment of web_static.
 
-dpkg -s nginx &> /dev/null
+# dpkg -s nginx &> /dev/null
 
-if [ $? -ne 0 ] ; then
+if [ "$(dpkg -s nginx &> /dev/null)" -ne 0 ] ; then
 	sudo apt update
 	sudo apt install nginx -y
 fi
 
-[ -d /data ]
-
-if [ $? -ne 0 ] ; then
+if ! [ -d /data ] ; then
 	sudo mkdir /data
 fi
 
-[ -d /data/web_static ]
-
-if [ $? -ne 0 ] ; then
+if ! [ -d /data/web_static ] ; then
 	sudo mkdir /data/web_static
 fi
 
-[ -d /data/web_static/releases ]
-
-if [ $? -ne 0 ] ; then
+if ! [ -d /data/web_static/releases ] ; then
 	sudo mkdir /data/web_static/releases
 fi
 
-[ -d /data/web_static/shared ]
-
-if [ $? -ne 0 ] ; then
+if ! [ -d /data/web_static/shared ] ; then
 	sudo mkdir /data/web_static/shared
 fi
 
-[ -d /data/web_static/releases/test/ ]
-
-if [ $? -ne 0 ] ; then
+if ! [ -d /data/web_static/releases/test ] ; then
 	sudo mkdir /data/web_static/releases/test/
 fi
 
-sudo echo "Hello World!!" > /data/web_static/releases/test/index.html
+sudo echo "Hello World!!" | /data/web_static/releases/test/index.html
 
-[ -L /data/web_static/current ]
-
-if [ $? -ne 0 ] ; then
+if ! [ -L /data/web_static/current ] ; then
 	sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 else
 	rm /data/web_static/current
