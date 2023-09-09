a97034e175371d24a6fcc1252d5ecf6ef315f5da
ZIHCO <chizobajames21@gmail.com>
2023-09-09 20:48:40 +0100

    deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,7 +3,9 @@
 from fabric.api import env
 do_deploy = __import__("2-do_deploy_web_static").do_deploy
 do_pack = __import__("1-pack_web_static").do_pack
-env.hosts = __import__("2-do_deploy_web_static").env.hosts
+# env.hosts = __import__("2-do_deploy_web_static").env.hosts
+
+env.hosts = ['100.26.121.248', '18.234.107.186']
 
 
 def deploy():
