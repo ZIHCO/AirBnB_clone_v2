b3b8c9d57c9b0e9c93d0337edd0d3f22f707cb25
ZIHCO <chizobajames21@gmail.com>
2023-09-09 15:24:20 +0100

    done

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+from fabric.api import task, lcd, local
+import tarfile
+import os
+from datetime import datetime
+
+
+@task
+def do_pack():
+    """Archive the contents of web-static into a .tgz file."""
+    directory = "versions"
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    now = datetime.now()
+    output = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
+    path = os.path.join(directory, output)
+    relative_path = os.path.join("../", path)
+    with lcd("web_static"):
+        local(f"tar -czf {relative_path} .")
+    if os.path.exists(path):
+        return path
