3a646b3f552f5ad84b7697474567ea0dd4616c3a
ZIHCO <chizobajames21@gmail.com>
2023-09-09 21:06:48 +0100

    deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""this module is fabfile"""
-from fabric.api import env
-do_deploy = __import__("2-do_deploy_web_static").do_deploy
-do_pack = __import__("1-pack_web_static").do_pack
-# env.hosts = __import__("2-do_deploy_web_static").env.hosts
+"""This module is a fabfile"""
+from fabric.api import put, run, cd, env, lcd, local
+import os
+import tarfile
+from datetime import datetime
 
 env.hosts = ['100.26.121.248', '18.234.107.186']
 
@@ -18,3 +18,45 @@ def deploy():
 
     deploy = do_deploy(path)
     return deploy
+
+
+def do_pack():
+    """Archive the contents of web-static into a .tgz file."""
+    directory = "versions"
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    now = datetime.now()
+    output = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
+    path = os.path.join(directory, output)
+    relative_path = os.path.join("../", path)
+    with lcd("web_static"):
+        local(f"tar -czf {relative_path} .")
+    if os.path.exists(path):
+        return path
+
+
+def do_deploy(archive_path):
+    """distributes an archive to my web servers"""
+    if not os.path.exists(archive_path):
+        return False
+    run('mkdir -p /tmp/')
+    put(archive_path, '/tmp/')  # upload this archive
+    archive_filename = archive_path.split("/")[1]
+    remote_path_archive = os.path.join("/tmp", archive_filename)
+    uncompress_file_dir = archive_filename.split(".")[0]
+    uncompress_file_path = os.path.join("/data/web_static/releases/",
+                                        uncompress_file_dir)
+    run("mkdir -p " + uncompress_file_path)  # enforce directory
+    with cd(uncompress_file_path):
+        run("tar xzvf " + remote_path_archive)
+
+    # delete archive file from web server
+    run("rm " + remote_path_archive)
+
+    # delete the symbolic link /data/web_static/current
+    run("rm /data/web_static/current")
+
+    # create symbolic link to uncompress_file_path
+    run("ln -s " + uncompress_file_path + " /data/web_static/current")
+
+    return True
