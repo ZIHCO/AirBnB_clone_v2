090edcc5e4022a8e7eaa9b553c3fa10bc4cf207c
ZIHCO <chizobajames21@gmail.com>
2023-09-23 10:26:18 +0100

    done

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""starts a flask web application"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """Home page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """hbnb page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """dynamic route"""
+    if "_" in text:
+        text_list = text.split("_")
+        text = " ".join(text_list)
+    string = "C " + text
+    return escape(string)
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+def python_route(text="is cool"):
+    """dynamic route"""
+    if "_" in text:
+        text_list = text.split("_")
+        text = " ".join(text_list)
+    string = "Python " + text
+    return escape(string)
+
+
+@app.route("/number/<text>", strict_slashes=False)
+def number_route(text):
+    """dynamic route"""
+    if type(int(text)) is int:
+        return "{} is a number".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
