a96a987f6bfbbde023b0e7907bca7b4311fb1ead
ZIHCO <chizobajames21@gmail.com>
2023-10-23 05:39:50 +0100

    add self to tear

--- models/place.py
+++ models/place.py
@@ -15,15 +15,17 @@ from sqlalchemy.orm import relationship
 from models.base_model import Base
 from models.base_model import BaseModel
 
-place_amenity = Table(
-    'place_amenity', Base.metadata,
-    Column(
-        'place_id', String(60), ForeignKey('places.id'), primary_key=True
-    ),
-    Column(
-        'amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True
-    )
-)
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+            Column(
+                'place_id', String(60), ForeignKey('places.id'), primary_key=True,
+                nullable=False
+                ),
+            Column(
+                'amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True,
+                nullable=False
+                )
+            )
 
 
 class Place(BaseModel, Base):
@@ -37,13 +39,12 @@ class Place(BaseModel, Base):
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, ColumnDefault(0), nullable=False)
-        number_bathrooms = Column(Integer, ColumnDefault(0), nullable=False)
-        max_guest = Column(Integer, ColumnDefault(0), nullable=False)
-        price_by_night = Column(Integer, ColumnDefault(0), nullable=False)
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        amenity_ids = []
         reviews = relationship(
             'Review', backref='place', cascade='all, delete, delete-orphan'
         )
